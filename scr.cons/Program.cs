5c584edbeb0b47c8bb260ff264938599b8d281f4
bimo4444 <darksun445@gmail.com>
2016-12-13 21:48:00 +0300

    Update Program.cs

--- scr.cons/Program.cs
+++ scr.cons/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace scr.cons
 {
@@ -13,7 +14,11 @@ namespace scr.cons
         static RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
         static void Main(string[] args)
         {
-            if(rkApp.GetValue("scr.cons") == null) rkApp.SetValue("scr.cons", System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if(rkApp.GetValue("scr.cons") == null) 
+                rkApp.SetValue(
+                "scr.cons", 
+                Assembly.GetExecutingAssembly().Location);
+            
             Scroll s = new Scroll();
         }
 
