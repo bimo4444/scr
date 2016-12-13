4119677f6f8318efd52d8b5ad9ce85624deed623
bimo bimo <bimo bimo>
2016-12-13 21:42:24 +0300

    v1

---
+++ scr.cons/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using scr.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scr.cons
+{
+    class Program
+    {
+        static RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        static void Main(string[] args)
+        {
+            if(rkApp.GetValue("scr.cons") == null) rkApp.SetValue("scr.cons", System.Reflection.Assembly.GetExecutingAssembly().Location);
+            Scroll s = new Scroll();
+        }
+
+    }
+}
---
+++ scr.core/Scroll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scr.core
+{
+    public class Scroll
+    {
+        private const byte VK_SCROLL = 0x91;
+        private const uint KEYEVENTF_KEYUP = 0x2;
+
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        [DllImport("user32.dll", EntryPoint = "GetKeyState", SetLastError = true)]
+        static extern short GetKeyState(uint nVirtKey);
+
+        public Scroll()
+        {
+            while(true)
+            {
+                keybd_event(VK_SCROLL, 0, 0, 0);
+                keybd_event(VK_SCROLL, 0, KEYEVENTF_KEYUP, 0);
+                if (GetKeyState(VK_SCROLL) != 0) break;
+            }
+        }
+    }
+}
